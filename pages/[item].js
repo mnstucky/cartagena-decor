9c732604cc4992fb631359b8d48f8248e240f4ef
Matt Stucky <mnstucky@gmail.com>
2021-01-24 14:46:17 -0600

    Begin implementing layout for individual item pages. Fix bug caused on hard refresh of item pages.

--- pages/[item].js
+++ pages/[item].js
@@ -3,22 +3,52 @@ import { useRouter } from "next/router";
 
 function ItemPage({ cart, setCart }) {
   const router = useRouter();
-  const itemUrl = router.query.item;
-  const [item, setItem] = useState("");
+  let itemUrl = router.query["item"];
+  const [item, setItem] = useState(undefined);
   useEffect(() => {
+    // Handle edge case where Next router isn't ready on complete app refresh
+    if (itemUrl == undefined) {
+      itemUrl = window.location.pathname.slice(1);
+    }
+    console.log(itemUrl);
     fetch(`/api/db?id=${itemUrl}`).then((res) => {
       if (!res.ok) {
         console.error("Network response wasn't ok");
       }
       res.json().then(function (data) {
+        console.log("useEffect triggered");
         setItem(data[0]);
       });
     });
   }, []);
-
-  return (
+  const gridStyles = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+  };
+  return item == undefined ? (
+    <div></div>
+  ) : (
     <div className="container">
-      <h1 className="title is-4 mt-2">{item.name}</h1>
+      <h1 className="title is-4 mt-2 has-text-centered">{item.name}</h1>
+      <div className="columns">
+        <div className="column">
+          <figure className="image box">
+            <img src={`/images/${item.images[0]}`} />
+          </figure>
+        </div>
+        <div className="column">
+          <section className="box content">
+            <p>{item.description}</p>
+            <p className="has-text-weight-bold">{item.highlights}</p>
+            {item.multiples.hasMultiples && (
+              <div class="select">
+                <select></select>
+              </div>
+            )}
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
