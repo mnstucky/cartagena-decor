8f36c1114dc6a7c3942c42236b540e1f8d8fca01
Matt Stucky <mnstucky@gmail.com>
2021-01-24 23:11:39 -0600

    Implement basic functionality for item pages. Create selection menus.

--- pages/[item].js
+++ pages/[item].js
@@ -1,51 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 function ItemPage({ cart, setCart }) {
   const router = useRouter();
-  let itemUrl = router.query["item"];
+  let itemUrl = router.query.item;
   const [item, setItem] = useState(undefined);
+  const [options, setOptions] = useState(undefined);
   useEffect(() => {
     // Handle edge case where Next router isn't ready on complete app refresh
-    if (itemUrl == undefined) {
+    if (itemUrl === undefined) {
       itemUrl = window.location.pathname.slice(1);
     }
-    console.log(itemUrl);
     fetch(`/api/db?id=${itemUrl}`).then((res) => {
       if (!res.ok) {
         console.error("Network response wasn't ok");
       }
-      res.json().then(function (data) {
-        console.log("useEffect triggered");
+      res.json().then((data) => {
         setItem(data[0]);
+        const optionsToSet = [];
+        for (const [key, value] of Object.entries(data[0].multiples.options)) {
+          if (value > 0) {
+            optionsToSet.push(key);
+          }
+        }
+        setOptions(optionsToSet);
       });
     });
   }, []);
-  const gridStyles = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-evenly",
-  };
-  return item == undefined ? (
-    <div></div>
+
+  return item === undefined || options === undefined ? (
+    <div />
   ) : (
     <div className="container">
       <h1 className="title is-4 mt-2 has-text-centered">{item.name}</h1>
       <div className="columns">
         <div className="column">
           <figure className="image box">
-            <img src={`/images/${item.images[0]}`} />
+            <img src={`/images/${item.images[0]}`} alt="Product for sale" />
           </figure>
         </div>
         <div className="column">
           <section className="box content">
             <p>{item.description}</p>
             <p className="has-text-weight-bold">{item.highlights}</p>
-            {item.multiples.hasMultiples && (
-              <div class="select">
-                <select></select>
-              </div>
-            )}
+            <div className="select">
+              <select>
+                {options.map((selection) => <option>{selection}</option>)}
+              </select>
+            </div>
+            <button className="button is-primary">Add to Cart</button>
           </section>
         </div>
       </div>
