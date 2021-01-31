98afe9f5ab90b8a8c89e2cf09d49f9e6243d1ec6
Matt Stucky <mnstucky@gmail.com>
2021-01-31 10:47:26 -0600

    Refactor out navbar links as separate components.

---
+++ components/NavbarLink.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+function NavbarLink({
+  toggleActive,
+  path,
+  label
+}) {
+  return (
+    <Link href={path}>
+      <a className="navbar-item" onClick={toggleActive}>
+        {label}
+      </a>
+    </Link>
+  );
+}
+
+export default NavbarLink;
--- components/NavbarMenu.js
+++ components/NavbarMenu.js
@@ -1,38 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import NavbarLink from './NavbarLink';
 
 function NavbarBrand({ isActive, toggleActive, cart }) {
   return (
     <div
       id="navbarBasicExample"
-      className={`navbar-menu ${isActive ? "is-active" : ""}`}
+      className={`navbar-menu ${isActive ? 'is-active' : ''}`}
     >
       <div className="navbar-start">
-        <Link href={"/"}>
-          <a className="navbar-item" onClick={toggleActive}>
-            Home
-          </a>
-        </Link>
-        <Link href={"/shop/"}>
-          <a className="navbar-item" onClick={toggleActive}>
-            Shop
-          </a>
-        </Link>
-        <Link href={"/contact/"}>
-          <a className="navbar-item" onClick={toggleActive}>
-            Contact
-          </a>
-        </Link>
-        <Link href={"/about/"}>
-          <a className="navbar-item" onClick={toggleActive}>
-            About
-          </a>
-        </Link>
+        <NavbarLink toggleActive={toggleActive} path="/" label="Home" />
+        <NavbarLink toggleActive={toggleActive} path="/shop/" label="Shop" />
+        <NavbarLink toggleActive={toggleActive} path="/contact/" label="Contact" />
+        <NavbarLink toggleActive={toggleActive} path="/about/" label="About" />
       </div>
       <div className="navbar-end">
         <div className="navbar-item">
           <div className="buttons">
-            <Link href={"/cart/"}>
+            <Link href="/cart/">
               <a className="button is-primary" onClick={toggleActive}>
                 <strong>{`Cart (${cart.reduce((acc, ele) => acc + ele.quantity, 0)})`}</strong>
               </a>
