6182736d8f89d203de51dd3e141180bcc1e1db21
Matt Stucky <mnstucky@gmail.com>
2021-02-19 20:59:44 -0600

    Fix typo in item page

--- pages/[item].js
+++ pages/[item].js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import ItemContainer from '../components/ItemContainer'
+import ItemContainer from '../components/ItemContainer';
 
 function ItemPage({
   cart,
