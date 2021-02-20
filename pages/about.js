4f456e160129492787346c6a0ae079ebd8a1aecf
Matt Stucky <mnstucky@gmail.com>
2021-02-19 21:28:51 -0600

    Update package.json for Heroku deployment

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function About() {
-  console.log(process.env.MONGO_URL);
   return (
     <div className="container">
       <h1 className="title is-4 mt-2">About Us</h1>
