3e31ca01f4bd82975cb58064a246109f6606a6fa
Matt Stucky <mnstucky@gmail.com>
2021-05-05 14:22:38 -0500

    Add functionality allowing admin to add main images for new item and upload them to S3

--- pages/admin/add.js
+++ pages/admin/add.js
@@ -23,7 +23,41 @@ function AddItem() {
   const [hasMultiples, setHasMultiples] = useState(false);
   const [options, setOptions] = useState(new Map());
   const [url, setUrl] = useState('');
+  const [locked, setLocked] = useState(false);
+  const [uploaded, setUploaded] = useState([]);
+  const [newUploadToggle, setNewUploadToggle] = useState(false);
   const { data: categories, error, loading } = useFetch('db?list=category');
+  function updateLocked(event) {
+    setLocked(true);
+  }
+  async function uploadMainImage(event) {
+    const file = event.target.files[0];
+    const convertedImageUrl = `${url}_main.JPG`;
+    const filename = encodeURIComponent(convertedImageUrl);
+    const response = await fetch(`/api/upload?file=${filename}`);
+    const { url: returnedUrl, fields } = await response.json();
+    const formData = new FormData();
+    Object.entries({ ...fields, file }).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    const upload = await fetch(returnedUrl, {
+      method: 'POST',
+      body: formData,
+    });
+    if (upload.ok) {
+      console.log('Uploaded successfully!');
+      if (!uploaded.includes(convertedImageUrl)) {
+        const newUploaded = [...uploaded];
+        newUploaded.push(convertedImageUrl);
+        setUploaded(newUploaded);
+      } else {
+        // Use toggle to force component rerender to update image query string and fetch new image
+        setNewUploadToggle((previousValue) => !previousValue);
+      }
+    } else {
+      console.error('Upload failed.');
+    }
+  }
   // TODO: Link authorized users to database
   if (session?.user?.email !== 'mnstucky@gmail.com') {
     return (
@@ -44,18 +78,53 @@ function AddItem() {
     return <Error />;
   }
   return (
-    <div className="container">
+    <div className="container pl-3 pr-3">
       <h1 className="is-size-4 has-text-weight-bold">Add Item</h1>
       <form>
-        <ControlledTextInput fieldName="Name" field={name} setField={setName} />
-        <ControlledSelect fieldName="Category" setField={setCategory} options={categories} />
-        <ControlledNumberInput fieldName="Stock" field={stock} setField={setStock} />
-        <ControlledNumberInput fieldName="Price" field={price} setField={setPrice} />
-        <ControlledTextareaSingle fieldName="Highlights" field={highlights} setField={setHighlights} />
-        <ControlledTextareaList fieldName="Description" fields={description} setFields={setDescription} />
-        <ControlledTextareaList fieldName="Features" fields={features} setFields={setFeatures} />
-        <ControlledMultiplesInput options={options} setOptions={setOptions} hasMultiples={hasMultiples} setHasMultiples={setHasMultiples} />
-        <ControlledUrlField url={url} setUrl={setUrl} />
+        {!locked
+            && (
+            <>
+              <ControlledTextInput fieldName="Name" field={name} setField={setName} />
+              <ControlledSelect fieldName="Category" setField={setCategory} options={categories} />
+              <ControlledNumberInput fieldName="Stock" field={stock} setField={setStock} />
+              <ControlledNumberInput fieldName="Price" field={price} setField={setPrice} />
+              <ControlledTextareaSingle fieldName="Highlights" field={highlights} setField={setHighlights} />
+              <ControlledTextareaList fieldName="Description" fields={description} setFields={setDescription} />
+              <ControlledTextareaList fieldName="Features" fields={features} setFields={setFeatures} />
+              <ControlledMultiplesInput options={options} setOptions={setOptions} hasMultiples={hasMultiples} setHasMultiples={setHasMultiples} />
+              <ControlledUrlField url={url} setUrl={setUrl} />
+              <button type="button" className="button is-primary" onClick={updateLocked}>
+                Save and Add Images
+              </button>
+            </>
+            )}
+        {locked
+        && (
+        <>
+          <label className="button is-primary">
+            Add Main Image
+            <input className="is-hidden" onChange={uploadMainImage} type="file" accept="image/jpg" />
+          </label>
+          <p>(Image must be in .JPG format. Preferred dimensions are 720 x 480.)</p>
+          <div className="columns">
+            {uploaded.map((imageUrl) => (
+              <div className="column is-one-third">
+                <figure className="image box">
+                  <img
+                    src={`https://cartagena-decor.s3.amazonaws.com/${imageUrl}?${Math.floor(Math.random() * 10000)}`}
+                    alt={`Uploaded at ${imageUrl}`}
+                  />
+                  <figcaption>
+                    Uploaded as:
+                    {' '}
+                    {imageUrl}
+                  </figcaption>
+                </figure>
+              </div>
+            ))}
+          </div>
+        </>
+        )}
       </form>
     </div>
 
