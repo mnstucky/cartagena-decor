1e2952be012085db68fe211e563d41f56f64d9fe
Matt Stucky <mnstucky@gmail.com>
2021-06-01 10:33:29 -0500

    Add mount check to useFetch hook

--- services/useFetch.js
+++ services/useFetch.js
@@ -1,27 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 export default function useFetch(url, needsRefresh) {
+  const isMounted = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // Use ref to check whether component is mounted before setting state
+    isMounted.current = true;
     async function getData() {
       try {
         const response = await fetch(`/api/${url}`);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          if (isMounted.current) setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        if (isMounted.current) setError(e);
       } finally {
-        setLoading(false);
+        if (isMounted.current) setLoading(false);
       }
     }
     getData();
+    // Function will run when component unmounts
+    return () => {
+      isMounted.current = false;
+    }
   }, [needsRefresh]);
 
   return { data, error, loading };
