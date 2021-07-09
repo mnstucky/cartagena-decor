51e5d833597136f8fa0cea22e5ab8a770d824b63
Matt Stucky <mnstucky@gmail.com>
2021-07-08 22:03:38 -0500

    Fix bug with checkout for items with multiple varieties

--- pages/api/createcheckout.js
+++ pages/api/createcheckout.js
@@ -1,28 +1,31 @@
-const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET);
+const stripe = require("stripe")(process.env.STRIPE_TEST_SECRET);
 // TODO: Update URL for production
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const cartContents = JSON.parse(req.body);
+    console.log(cartContents);
     const formattedContents = cartContents.map((item) => ({
       price_data: {
-        currency: 'usd',
+        currency: "usd",
         product_data: {
-          name: item.name,
+          name: `${item.name} ${
+            item.option !== "default" ? `| ${item.option}` : ""
+          }`,
         },
         unit_amount: item.price * 100,
       },
       quantity: item.quantity,
     }));
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+      payment_method_types: ["card"],
       // TODO: Incorporate additional logic to handle different shipping rates, if desired
-      shipping_rates: ['shr_1Iq8XsAh1yeccWEtQ3p0oalt'],
+      shipping_rates: ["shr_1Iq8XsAh1yeccWEtQ3p0oalt"],
       shipping_address_collection: {
-        allowed_countries: ['US'],
+        allowed_countries: ["US"],
       },
       line_items: formattedContents,
-      mode: 'payment',
+      mode: "payment",
       success_url: `${process.env.NEXTAUTH_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.NEXTAUTH_URL}/cart`,
     });
