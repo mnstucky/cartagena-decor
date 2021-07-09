452fbae02edc18172e5807920f2eae1937d7ed44
Matt Stucky <mnstucky@gmail.com>
2021-07-08 22:09:34 -0500

    Reverse sort of orders

--- pages/api/getallorders.js
+++ pages/api/getallorders.js
@@ -1,31 +1,39 @@
-import { getSession } from 'next-auth/client';
-import getAdmins from '../../services/getAdmins';
+import { getSession } from "next-auth/client";
+import getAdmins from "../../services/getAdmins";
 
-const mongoose = require('mongoose');
-const Order = require('../../services/orders.js');
+const mongoose = require("mongoose");
+const Order = require("../../services/orders.js");
 
 export default async function handler(req, res) {
   // Validate admin user
   const session = await getSession({ req });
   const admins = await getAdmins();
   if (!admins.some((admin) => admin.email === session?.user?.email)) {
-    res.json({ error: 'You must be signed in as an administrator to retrieve all orders.' });
+    res.json({
+      error:
+        "You must be signed in as an administrator to retrieve all orders.",
+    });
     return;
   }
   // Connect to DB
-  await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  await mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .catch((err) => console.error(err));
   // Get notifications of connection errors
-  mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "MongoDB connection error: ")
+  );
   // Handle requests
   const { query } = req;
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     // If no parameter is used, return all orders
     if (Object.keys(query).length === 0) {
       Order.find({})
+        .sort({ date: -1 })
         .exec((err, orders) => {
           const formattedOrders = JSON.stringify(orders);
           res.json(formattedOrders);
--- pages/api/getorders.js
+++ pages/api/getorders.js
@@ -1,28 +1,33 @@
-import { getSession } from 'next-auth/client';
+import { getSession } from "next-auth/client";
 
-const mongoose = require('mongoose');
-const Order = require('../../services/orders.js');
+const mongoose = require("mongoose");
+const Order = require("../../services/orders.js");
 
 export default async function handler(req, res) {
   // Get user info
   const session = await getSession({ req });
   // Connect to DB
-  await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  await mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .catch((err) => console.error(err));
   // Get notifications of connection errors
-  mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "MongoDB connection error: ")
+  );
   // Handle requests
   const { query } = req;
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     // If no id parameter is used, return all orders for signed-in user by email address
     if (!session?.user) {
-      res.json({ error: 'You must be signed in to access this API route.' });
+      res.json({ error: "You must be signed in to access this API route." });
     }
     if (Object.keys(query).length === 0) {
       Order.find({ email: session.user.email })
+        .sort({ date: -1 })
         .exec((err, orders) => {
           const formattedOrders = JSON.stringify(orders);
           res.json(formattedOrders);
