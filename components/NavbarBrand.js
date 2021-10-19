ce4908674ec7281debb4fe714b571a442cd0bd07
Matthew Stucky <matthew.stucky@lptech.com>
2021-10-18 22:17:01 -0500

    Update with Maran Cafe details

--- components/NavbarBrand.js
+++ components/NavbarBrand.js
@@ -3,26 +3,40 @@ import Link from 'next/link';
 
 function NavbarBrand({ isActive, toggleActive }) {
   return (
-    <div className="navbar-brand">
-      <Link href="/">
-        <a className="navbar-item">
-          <img alt="Cartagena Decor logo" src="https://cartagena-decor.s3.amazonaws.com/logo.jpg" width="28" height="28" />
-          <h1 className="is-size-4 is-size-5-mobile is-uppercase is-family-secondary ml-2 has-text-black">
-            Cartagena Decor
+    <div className='navbar-brand'>
+      <Link href='/'>
+        <a className='navbar-item'>
+          <img
+            alt='Cartagena Decor logo'
+            src='https://cartagena-decor.s3.amazonaws.com/logo.jpg'
+            width='28'
+            height='28'
+          />
+          <h1 className='is-size-4 is-hidden-mobile is-uppercase is-family-secondary ml-2 has-text-black'>
+            cartagena decor
           </h1>
+          <h1 className='is-size-4 is-size-5-mobile is-uppercase is-family-secondary ml-2 has-text-black'>
+            +
+          </h1>
+          <img
+            alt='Maran Cafe logo'
+            src='https://cartagena-decor.s3.amazonaws.com/cropped-Logo-parte-superior-1.png'
+            height='28'
+            className='ml-2'
+          />
         </a>
       </Link>
       <a
-        role="button"
+        role='button'
         className={`navbar-burger ${isActive ? 'is-active' : ''}`}
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
+        aria-label='menu'
+        aria-expanded='false'
+        data-target='navbarBasicExample'
         onClick={toggleActive}
       >
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
+        <span aria-hidden='true' />
+        <span aria-hidden='true' />
+        <span aria-hidden='true' />
       </a>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,27 @@ import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <div className="container pr-3 pl-3">
-      <div className="block">
+    <div className='container pr-3 pl-3'>
+      <div className='block'>
         <Hero />
       </div>
-      <div className="block">
-        <h1 className="is-size-3 is-size-5-mobile is-uppercase is-family-secondary ml-2 has-text-black">Welcome to Cartagena Decor!</h1>
+      <div className='block'>
+        <h1 className='is-size-3 is-size-5-mobile is-uppercase is-family-secondary ml-2 has-text-black'>
+          Welcome to Cartagena Decor and Maran Caf&eacute;!
+        </h1>
       </div>
-      <div className="block">
-        <p className="is-size-5-desktop">Established in 2016, Cartagena Decor sells unique and authentic handcrafted-wood products inspired by Colombian folk art and perfect for decoration and for serving your favorite foods.</p>
+      <div className='block'>
+        <p className='is-size-5-desktop'>
+          Established in 2016, Cartagena Decor LLC works with coffee producers
+          and local artists in Colombia through direct and fair trade practices.
+          Cartagena Decor is the authorized distributor of Maran Caf&eacute;,
+          single-origin specialty Colombian coffee.
+        </p>
       </div>
-      <div className="block">
-        <p className="is-size-5-desktop">Check out our full range of products!</p>
+      <div className='block'>
+        <p className='is-size-5-desktop'>
+          Check out our full range of products!
+        </p>
       </div>
     </div>
   );
