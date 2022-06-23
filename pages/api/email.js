9554b0c3707decc52d9325fb9363bc889a9a0a07
Matt Stucky <mnstucky@gmail.com>
2022-06-22 22:49:15 -0500

    Change email route to use nodemailer

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,16 +1,20 @@
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const nodemailer = require("nodemailer");
 
 export default function handler(req, res) {
   if (req.method === "POST") {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.mail.yahoo.com",
+      port: 587,
+      secure: false,
+      auth: { user: process.env.EMAIL_SERVER_USER, pass: process.env.EMAIL_SERVER_PASSWORD },
+    });
     const msg = {
-      to: "info@cartagenadecor.com", // Update SendGrid to include l
-      from: "info@cartagenadecor.com", // same
+      to: process.env.EMAIL_FROM,
+      from: "cartagenadecor@yahoo.com",
       subject: "Contact Form for Cartagena Decor",
       html: `<h2>Message from <strong>${req.body.name}</strong>:</h2></br><p>"${req.body.subject}"</p></br><p>Respond to ${req.body.email}</p>`,
     };
-    sgMail.send(msg).then(
+    transporter.sendMail(msg).then(
       () => {
         res.json({
           status: "success",
