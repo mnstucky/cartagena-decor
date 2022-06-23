4728954569c4ea84cdd306dc1cde5108afd87814
Matthew Stucky <matthew.stucky@lptech.com>
2022-06-23 12:56:01 -0500

    Change email address for contact form

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,23 +1,26 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.mail.yahoo.com",
+  if (req.method === 'POST') {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.mail.yahoo.com',
       port: 587,
       secure: false,
-      auth: { user: process.env.EMAIL_SERVER_USER, pass: process.env.EMAIL_SERVER_PASSWORD },
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+      },
     });
     const msg = {
-      to: process.env.EMAIL_FROM,
-      from: "cartagenadecor@yahoo.com",
-      subject: "Contact Form for Cartagena Decor",
+      to: 'cartagenadecor@gmail.com',
+      from: 'cartagenadecor@yahoo.com',
+      subject: 'Contact Form for Cartagena Decor',
       html: `<h2>Message from <strong>${req.body.name}</strong>:</h2></br><p>"${req.body.subject}"</p></br><p>Respond to ${req.body.email}</p>`,
     };
     transporter.sendMail(msg).then(
       () => {
         res.json({
-          status: "success",
+          status: 'success',
         });
       },
       (error) => {
@@ -27,7 +30,7 @@ export default function handler(req, res) {
           console.error(error.response.body);
         }
         res.json({
-          status: "fail",
+          status: 'fail',
         });
       }
     );
